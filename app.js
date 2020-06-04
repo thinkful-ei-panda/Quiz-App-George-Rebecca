adcbf46e82e49214ebb587cafb127dfd6ea68a9b
Rebecca <rhudson.813@gmail.com>
2020-06-04 02:20:01 -0400

    added quizAppXtras and function stubs to .js

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
-/**
- * Example store structure
- */
-const store = {
-  // 5 or more questions are required
-  questions: [
+'use strict';
+
+const musicTrivia = [
     {
-      question: 'What color is broccoli?',
+      question: '',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
       ],
-      correctAnswer: 'green'
+      correctAnswer: ''
     },
     {
-      question: 'What is the current year?',
+      question: '',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
+        ],
+      correctAnswer: ''
     }
   ],
   quizStarted: false,
@@ -34,7 +23,7 @@ const store = {
  * 
  * Technical requirements:
  * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
+ * Your app should include a render() function, that regenerates the view each time the quiz is updated. 
  * See your course material, consult your instructor, and reference the slides for more details.
  *
  * NO additional HTML elements should be added to the index.html file.
@@ -48,11 +37,64 @@ const store = {
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
+function genIntro() {
+  // generates intro page function
+}
+
+function genQuestion() {
+  // generates question page
+}
+
+function genCorrect() {
+  // generates correct answer pop-up
+}
+
+function genIncorrect() {
+  // generates incorrect answer pop-up
+}
+
+function genResults() {
+  // generates result pop-up
+}
 
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
+function renderIntro() {
+  // renders intro page through genIntro function
+}
+
+function renderQuestion() {
+  // renders randomly selected new question through genQuestion function
+}
+
+function renderCorrect() {
+  // renders updates to add to score through genCorrect function
+}
+
+function renderIncorrect() {
+  // renders updates score through genIncorrect function
+}
+
+function renderResults() {
+  // renders results from genResults function
+}
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+function handleStart() {
+  // event handle when click begin button => renderQuestin function
+}
+
+function handleSubmit() {
+  // event handle when click submit button - if answer is correct =>  renderCorrect add to score - if answer is incorrect => renderIncorrect
+}
+
+function handleNext() {
+  // event handle when click next button - if more questions => go to next question add to questionNumber - if done with quiz => go to results page
+}
+
+function handleNewQuiz() {
+  // event handle when click encore button => renderIntro function
+}
